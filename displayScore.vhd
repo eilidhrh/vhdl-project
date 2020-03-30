10029679be97d44056385c693dd8df3494aea57f
eilidhrh <61511620+eilidhrh@users.noreply.github.com>
2020-03-30 11:18:57 +0100

    Added count and display processes

--- displayScore.vhd
+++ displayScore.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -35,12 +35,44 @@ entity displayScore is
     Port ( s1 : in STD_LOGIC_VECTOR (4 downto 0);
            s2 : in STD_LOGIC_VECTOR (4 downto 0);
            clk : in STD_LOGIC;
-           segment7 : out STD_LOGIC);
+           anode: out STD_LOGIC_VECTOR(3 downto 0);
+           segments: out STD_LOGIC_VECTOR(6 downto 0));
 end displayScore;
 
 architecture V1 of displayScore is
 
+signal activate_counter : unsigned(1 downto 0) := "00";
+
 begin
 
+
+--Every clock pulse will change which LED is being activated
+count : process(clk)
+begin
+    if(rising_edge(clk)) then
+        activate_counter <= activate_counter + 1;
+    end if;
+end process;
+
+--Every time the activate_counter changes will activate the correct LED
+--Will also change the segment so the correct number is displayed
+display: process(activate_counter)
+begin
+    case activate_counter is
+    when "00" =>
+        anode <= "0111";
+    when "01" =>
+        anode <= "1011";
+    when "10" =>
+        anode <= "1101";
+    when "11" =>
+        anode <= "1110";
+    end case;
+end process;
+
+findInt1 : process(s1)
+begin
+    
+end process;
 
 end V1;
