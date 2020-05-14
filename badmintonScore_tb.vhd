36a73ecee5ad8f80a2031d11f356514d40acefd0
martin-dimov <62427781+martin-dimov@users.noreply.github.com>
2020-05-14 08:53:39 +0100

    completed

--- badmintonScore_tb.vhd
+++ badmintonScore_tb.vhd
@@ -1,35 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 07.05.2020 09:58:09
--- Design Name: 
--- Module Name: badmintonScore_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity badmintonScore_tb is
 --  Port ( );
@@ -65,15 +37,13 @@ clockgen : process
 
 stimuli: process
 	begin
-	 p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 10ns;    --use this for each test
+	
+	 --initialise score
+	 p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 10ns;    --use this for each test	 
 	 
-	--expand me for tests
-	--test 1 - buttons only
-	  --initialises score
-       
-        
---        --adds a couple points to each score
---        p1btn <= '1'; wait for 10ns;
+-----------TEST 1 - buttons only--------------------------------------------
+	
+--        p1btn <= '1'; wait for 10ns;                     --adds a couple points to each score
 --        p2btn <= '1'; wait for 10ns;
 --        p1btn<= '0'; p2btn <= '0'; wait for 10ns;
 --        p1btn <= '1'; wait for 10ns;
@@ -88,124 +58,134 @@ stimuli: process
 --        p1btn <= '0'; p2btn <= '0'; wait for 10 ns;
 --        p1btn <= '1'; p2btn <= '1'; wait for 10 ns;
 --        p1btn <= '1'; p2btn <= '0'; wait for 10 ns;      --release p2 first
---        p1btn <= '0'; p2btn <= '0'; wait for 10 ns;
+--        p1btn <= '0'; p2btn <= '0'; wait for 40 ns;      --wait full cycle of anode
+--        reset <= '1'; wait for 10 ns;
+--        reset <= '0'; wait for 40 ns;                    --reset score and wait for anode to update   
+        
+--        for i in 0 to 16 loop                            --run up score to 17:17
+--         p1btn <= '1'; wait for 10ns;
+--         p1btn <= '0'; wait for 10ns;
+--         p2btn <= '1'; wait for 10ns;
+--         p2btn <= '0'; wait for 10ns;
+       
+--        end loop;
+        
+--        p1btn <= '1'; wait for 10 ns;
+--        p1btn <= '0'; wait for 10 ns;
+--        p1btn <= '1'; wait for 10 ns;
+--        p1btn <= '0'; wait for 10 ns;                    --have player 1 win, observe pattern
+--        p1btn <= '1'; wait for 10 ns;
+--        p1btn <= '0'; wait for 70 ns;                    --hold to check segments
+--        p1btn <= '1'; wait for 10 ns;
+--        p1btn <= '0'; wait for 10 ns;
+--                                                           -- uncomment either of these depending on p1 or p2 to win
+--        p2btn <= '1'; wait for 10 ns;
+--        p2btn <= '0'; wait for 10 ns;
+--        p2btn <= '1'; wait for 10 ns;
+--        p2btn <= '0'; wait for 10 ns;                    --have player 2 win, observe pattern
+--        p2btn <= '1'; wait for 10 ns;
+--        p2btn <= '0'; wait for 10 ns;
+--        p2btn <= '1'; wait for 10 ns;
+--        p2btn <= '0'; wait for 10 ns;                    --don't forget final wait statement
+        
+----------------------------------------------------------------------------	     
 
-----------------------------------------------------------------------------	
-	
+-----------TEST 2 - testing reset and margin--------------------------------
+  
+--        p1btn <= '1'; p2btn <= '0'; reset <= '1'; wait for 20ns;        --reset preventing p1 or p2
+--        p1btn <= '1'; p2btn <= '1'; reset <= '1'; wait for 20ns;
+--        p1btn <= '0'; p2btn <= '1'; reset <= '1'; wait for 20ns;
+--        p1btn <= '1'; p2btn <= '1'; reset <= '1'; wait for 20ns;        
+--        p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 40ns;        --wait full cycle of anode
+ 
+--        p1btn <= '1'; p2btn <= '1'; reset <= '0'; wait for 40ns;        -- wait full cycle of anode
         
-  	
-	--test 2 - testing reset
---	  --initialises score
---        p1btn <= '0'; p2btn <='0'; reset <='0'; wait for 10ns; 
-    
---        p1btn <= '1'; reset <= '1'; wait for 10ns;        --reset preventing p1 or p2
---        p2btn <= '1'; wait for 10ns;
---        p1btn <= '0'; p2btn <= '0'; wait for 40ns;        --wait full cycle of anode
+--        for i in 0 to 25 loop                                           --run score up to 26:26 to check margin win condition             
+--          p1btn <= '1'; wait for 10ns;  
+--          p1btn <= '0'; wait for 10ns;
+--          p2btn <= '1'; wait for 10ns;
+--          p2btn <= '0'; wait for 10ns;
+--        end loop;
         
---        p1btn <= '1'; p2btn <= '1'; reset <= '0'; wait for 10ns;
---        p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 10ns;
---        p1btn <= '1'; p2btn <= '1'; reset <= '0'; wait for 10ns;
---        p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 10ns;
---        p1btn <= '1'; p2btn <= '1'; reset <= '0'; wait for 58 ns;            --add a few points, wait full cycle and reset right before clock edge
---        p1btn <= '0'; p2btn <= '0'; reset <= '1'; wait for 10ns;
---        reset <= '0'; 
+--          p1btn <= '1'; wait for 10 ns;
+--          p1btn <= '0'; wait for 60 ns;
+--          p1btn <= '1'; wait for 10 ns;                                 --player 1 should win on 28:26
+--          p1btn <= '0'; wait for 10 ns;
+--                                                                uncomment either for p1 or p2
+--          p2btn <= '1'; wait for 10 ns;
+--          p2btn <= '0'; wait for 60 ns;                               --player 2 should win on 26:28
+--          p2btn <= '1'; wait for 10 ns;                                 
+--          p2btn <= '0'; wait for 10 ns;
+
 ----------------------------------------------------------------------------
 
 
---------TEST 3 - win 1, reset, win 1 again, wait full 5 seconds------------------
+--------TEST 3 - win 1, reset, win 1 again, wait full 5 seconds-------------
         
-       --initialises score
---          p1btn <= '0'; p2btn <= '0'; reset <= '0'; wait for 10ns;
-          
---          for i in 0 to 19 loop
---            p1btn <= '1'; wait for 5ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-          
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 125 ns;
---            reset <= '1'; wait for 20 ns;
---            reset <= '0'; wait for 80 ns;
-            
---           for i in 0 to 19 loop
---            p1btn <= '1'; wait for 10ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-        
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 10 ns;
-
-----------------------------------------------------------------------------        
-
-          
---          for i in 0 to 19 loop
---            p1btn <= '1'; wait for 5ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-          
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 125 ns;
---            reset <= '1'; wait for 20 ns;
---            reset <= '0'; wait for 80 ns;
-            
---           for i in 0 to 19 loop
---            p1btn <= '1'; wait for 10ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-        
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 10 ns;
-----------------------------------------------------------------------------
-
-
-----------------TEST 4 - first p1 wins then p2
-
---          for i in 0 to 19 loop
---            p1btn <= '1'; wait for 5ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-          
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 125 ns;
---            reset <= '1'; wait for 20 ns;
---            reset <= '0'; wait for 80 ns;
-            
---           for i in 0 to 19 loop
---            p1btn <= '1'; wait for 10ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-        
---            p2btn <= '1'; wait for 10 ns;
---            p2btn <= '0'; wait for 10 ns;
-
-
-
-
-
---------------TEST 5 - repeat test 4 without reset
-
---          for i in 0 to 19 loop
---            p1btn <= '1'; wait for 5ns;
---            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
---            p2btn <= '0'; wait for 10ns;
---          end loop;
-          
---            p1btn <= '1'; wait for 10 ns;
---            p1btn <= '0'; wait for 500 ns;
       
+--          for i in 0 to 19 loop                     --get to 20:20
+--            p1btn <= '1'; wait for 5ns;
+--            p1btn <= '0'; wait for 10ns;
+--            p2btn <= '1'; wait for 10ns;
+--            p2btn <= '0'; wait for 10ns;
+--          end loop;
+          
+--            p1btn <= '1'; wait for 10 ns;
+--            p1btn <= '0'; wait for 10 ns;
+--            p1btn <= '1'; wait for 10 ns;          -- win on 22:20 and wait for pattern
+--            p1btn <= '0'; wait for 125 ns;
+--            reset <= '1'; wait for 20 ns;           -- reset    
+--            reset <= '0'; wait for 80 ns;
+
+--            p2btn <= '1'; wait for 10 ns;
+--            p2btn <= '0'; wait for 10 ns;
+--            p2btn <= '1'; wait for 10 ns;          -- same code if p2 win
+--            p2btn <= '0'; wait for 125 ns;
+--            reset <= '1'; wait for 20 ns;           -- reset    
+--            reset <= '0'; wait for 80 ns;
+
+
+
+            
+--           for i in 0 to 19 loop                    -- repeat
+--            p1btn <= '1'; wait for 10ns;
+--            p1btn <= '0'; wait for 10ns;
+--            p2btn <= '1'; wait for 10ns;
+--            p2btn <= '0'; wait for 10ns;
+--          end loop;
+        
+--            p1btn <= '1'; wait for 10 ns;
+--            p1btn <= '0'; wait for 10 ns;
+--            p1btn <= '1'; wait for 10 ns;           --code for p1 win
+--            p1btn <= '0'; wait for 10 ns;
+
+--            p2btn <= '1'; wait for 10 ns;           --same code if p2 win
+--            p2btn <= '0'; wait for 10 ns;
+--            p2btn <= '1'; wait for 10 ns;
+--            p2btn <= '0'; wait for 10 ns;
+
+----------------------------------------------------------------------------
+
+
+----------------TEST 4 - first p1 wins then p2------------------------------
+
+--          for i in 0 to 19 loop
+--            p1btn <= '1'; wait for 10ns;
+--            p1btn <= '0'; wait for 10ns;
+--            p2btn <= '1'; wait for 10ns;
+--            p2btn <= '0'; wait for 10ns;
+--          end loop;
+          
+--            p1btn <= '1'; wait for 10 ns;
+--            p1btn <= '0'; wait for 10 ns;
+            
+--            p1btn <= '1'; wait for 10 ns;
+--            p1btn <= '0'; wait for 125 ns;          --wait for sequence
+          
+--            reset <= '1'; wait for 20 ns;           --reset
+--            reset <= '0'; wait for 80 ns;
+            
+            
 --           for i in 0 to 19 loop
 --            p1btn <= '1'; wait for 10ns;
 --            p1btn <= '0'; wait for 10ns;
@@ -214,38 +194,112 @@ stimuli: process
 --          end loop;
         
 --            p2btn <= '1'; wait for 10 ns;
+--            p2btn <= '0'; wait for 10 ns;           --2 wins
+--            p2btn <= '1'; wait for 10 ns;
 --            p2btn <= '0'; wait for 10 ns;
-            
-            
- --------------TEST 6 - check score isn't being added during w1
+
+----------------------------------------------------------------------------
+
+
+--------------TEST 5 - repeat test 4 without reset and reversed-------------
+
+--          for i in 0 to 18 loop
+--            p1btn <= '1'; wait for 10ns;
+--            p1btn <= '0'; wait for 10ns;            
+--            p2btn <= '1'; wait for 10ns;
+--            p2btn <= '0'; wait for 10ns;
+--          end loop;
+          
+--            p2btn <= '1'; wait for 10 ns;
+--            p2btn <= '0'; wait for 50 ns;               -- wait for score
+--            p2btn <= '1'; wait for 10 ns;
+--            p2btn <= '0'; wait for 500 ns;              --p2 win and wait 500 ns for sequence
+        
+--           for i in 0 to 18 loop
+--            p1btn <= '1'; wait for 10ns;
+--            p1btn <= '0'; wait for 10ns;                
+--            p2btn <= '1'; wait for 10ns;
+--            p2btn <= '0'; wait for 10ns;
+--          end loop;
+        
+--            p1btn <= '1'; wait for 10 ns;
+--            p1btn <= '0'; wait for 50 ns;               --wait for score
+--            p1btn <= '1'; wait for 10 ns;               --p1 win
+--            p1btn <= '0'; wait for 10 ns;
+
+----------------------------------------------------------------------------           
+        
+        
+ --------------TEST 6 - check score isn't being added during w1--------------
 
 --          for i in 0 to 19 loop
---            p1btn <= '1'; wait for 5ns;
+--            p1btn <= '1'; wait for 10ns;
 --            p1btn <= '0'; wait for 10ns;
 --            p2btn <= '1'; wait for 10ns;
 --            p2btn <= '0'; wait for 10ns;
 --          end loop;
           
---           for i in 0 to 26 loop
---            p1btn <= '1'; wait for 5ns;
---            p1btn <= '0'; wait for 5ns;
+--          p2btn <= '1'; wait for 10ns;
+--          p2btn <= '0'; wait for 10ns;
+--          p2btn <= '1'; wait for 10ns;
+--          p2btn <= '0'; wait for 10ns;                  --player 2 wins
+          
+--           for i in 0 to 18 loop
+--            p1btn <= '1'; wait for 5ns; 
+--            p1btn <= '0'; wait for 5ns;                 --score  points during sequence
 --            p2btn <= '1'; wait for 5ns;
---            p2btn <= '0'; wait for 5ns;
+--           p2btn <= '0'; wait for 5ns;
 --          end loop;
             
        
---         -- wait for 400 ns;  -- waiting for led sequence to complete
+--          wait for 400 ns;  -- waiting for led sequence to complete
           
---          for i in 0 to 19 loop
+--          for i in 0 to 9 loop
 --            p1btn <= '1'; wait for 10ns;
 --            p1btn <= '0'; wait for 10ns;
---            p2btn <= '1'; wait for 10ns;
+--          end loop;
+          
+--          for i in 0 to 9 loop
 --            p2btn <= '0'; wait for 10ns;
---         end loop;
+--            p2btn <= '1'; wait for 10ns;
+--          end loop;
+          
+--          for i in 0 to 9 loop
+--            p2btn <= '0'; wait for 10ns;
+--            p2btn <= '1'; wait for 10ns;
+--          end loop;
         
---            p2btn <= '1'; wait for 10 ns;
---            p2btn <= '0'; wait for 10 ns;
+           
+--          p2btn <= '1'; wait for 10ns;
+--          p2btn <= '0'; wait for 10ns;
+--          p2btn <= '1'; wait for 10ns;
+--          p2btn <= '0'; wait for 10ns;
+         
+----------------------------------------------------------------------------
+        
+        
+ --------------------Test 7 - checking >1 margin and going to 30------------
+     
+--        for i in 0 to 28 loop
+--           p1btn <= '1'; wait for 10ns;
+--           p1btn <= '0'; wait for 10ns;     --player 1
+--           p2btn <= '1'; wait for 10ns;
+--           p2btn <= '0'; wait for 10ns;
+--        end loop;    
+            
+--        p1btn <= '1'; wait for 10ns;
+--        p1btn <= '0'; wait for 10ns;
 
+
+        for i in 0 to 28 loop                       
+           p1btn <= '1'; wait for 10ns;
+           p1btn <= '0'; wait for 10ns;     --player 2
+           p2btn <= '1'; wait for 10ns;
+           p2btn <= '0'; wait for 10ns;
+        end loop;    
+            
+           p2btn <= '1'; wait for 10ns;
+           p2btn <= '0'; wait for 10ns;
 	
 	
 	wait;
